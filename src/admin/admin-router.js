4b05c781f22c27c571920150663a53f4b56fcd2a
Raul Barrera <rbarrera1992@gmail.com>
2021-01-31 14:06:41 -0600

    delete category working

--- src/admin/admin-router.js
+++ src/admin/admin-router.js
@@ -14,33 +14,70 @@ adminRouter
       error: `Missing 'category' in request body`
     })
 
-    console.log(category)
     try {
-      if(req.user.admin) {
-        await AdminService.postCategory(
-          req.app.get('db'),
-          category
-        )
-          
-        res.send({
-          status: `category '${category}' created`
-        })
-      } else {
-        res.status(400).json({
+      if(!req.user.admin){
+        return res.status(400).json({
           error: 'Not an admin'
         })
       }
+
+      const hasCategory = await AdminService.hasCategory(
+        req.app.get('db'),
+        category
+      )
+
+      if(hasCategory)
+        return res.status(400).json({ error: 'Category already exists'})
+
+      
+      await AdminService.postCategory(
+        req.app.get('db'),
+        category
+      )
+          
+      res.send({
+        status: `category '${category}' created`
+      })
+      
     } catch (error) {
       next(error)
     }
   })
-//   .delete('/category', async (req, res, next) => {
-//     try {
+  .delete('/category', bodyParser, async (req, res, next) => {
+    const { id } = req.body
 
-//     } catch (error) {
-//       next(error)
-//     }
-//   })
+    if(id == null) return res.status(400).json({
+      error: `Missing 'id' in request body`
+    })
+    
+    try {
+      if(!req.user.admin){
+        return res.status(400).json({
+          error: 'Not an admin'
+        })
+      }
+
+      const hasCategoryId = await AdminService.hasCategoryId(
+        req.app.get('db'),
+        id
+      )
+
+      if(!hasCategoryId)
+        return res.status(400).json({ error: 'Category Id does not exist'})
+
+      
+      await AdminService.deleteCategory(
+        req.app.get('db'),
+        id
+      )
+          
+      res.send({
+        status: `category with id '${id}' deleted`
+      })
+    } catch (error) {
+      next(error)
+    }
+  })
 //   .put('/category', async (req, res, next) => {
 //     try {
 
--- src/admin/admin-service.js
+++ src/admin/admin-service.js
@@ -1,10 +1,29 @@
 const AdminService = {
+  hasCategory(db, category){
+    return db('category')
+      .where({ category })
+      .first()
+      .then(category => !!category)
+  },
+
+  hasCategoryId(db, id){
+    return db('category')
+      .where({ id })
+      .first()
+      .then(id => !!id)
+  },
+
   postCategory(db, category){
-    console.log(category)
     return db
       .insert({ 'category': category })
       .into('category')
   },
+
+  deleteCategory(db, id) {
+    return db('category')
+      .delete()
+      .where({id})
+  }
 }
 
 module.exports = AdminService
